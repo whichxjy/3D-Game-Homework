bccb7c7b52cbd8b6681f504b29c0fc8650214f51
whichxjy <whichxjy@gmail.com>
2019-09-21 22:27:46 +0800

    Update

---
+++ HW3/Solar-System/Assets/Scripts/Revolution.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Revolution : MonoBehaviour {
+    public Transform sun;
+    private float speed;
+    private Vector3 axis;
+
+    void Start() {
+        speed = Random.Range(10f, 30f);
+        axis = new Vector3(Random.Range(-30f, 30f), Random.Range(-30f, 30f), 0f);
+    }
+
+    void Update() {
+        // revolution around the Sun
+        transform.RotateAround(sun.position, axis, speed * Time.deltaTime);
+    }
+}
---
+++ HW3/Solar-System/Assets/Scripts/Rotation.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+    void Update() {
+        transform.RotateAround(transform.position, Vector3.up, Random.Range(1f, 5f));
+    }
+}
